e3ce142b2f89a84134acd3112bba0a56d014252b
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-06-26 11:43:00 +0300

    Adds index.html

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (a, b) => {
+    return a / b;
+  };
+  
+  const square = (x) => {
+    return x * x;
+  };
+  
+  const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
